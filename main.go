37373ffc5cbdc33bdf784211142605f03766c248
Leonardo-Antonio <leo2001.nl8@gmail.com>
2020-12-26 20:25:24 -0500

    port

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"os"
+	//"os"
 )
 
 func main() {
@@ -12,5 +13,8 @@ func main() {
 	e.Use(middleware.CORS())
 	router.Image(e)
 	port := os.Getenv("POST")
+	if port == "" {
+		port = ":8080"
+	}
 	e.Logger.Fatal(e.Start(port))
 }
