193ab6995ee57fa5a6c6f24cc32da60ec679fbdc
Leonardo-Antonio <leo2001.nl8@gmail.com>
2020-12-26 20:34:57 -0500

    port

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/Leonardo-Antonio/api-convert-to-b64/router"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -16,5 +17,6 @@ func main() {
 	if port == "" {
 		port = ":8080"
 	}
+	fmt.Println("PORTTTTTTTTTTTTT", port)
 	e.Logger.Fatal(e.Start(port))
 }
